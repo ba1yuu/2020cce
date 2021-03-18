816870b2836d8f93dfda70266eb5285b99a6aba3
ba1yuu <45198662+ba1yuu@users.noreply.github.com>
2021-03-18 15:45:39 +0800

    Add files via upload

---
+++ week03/hw1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int a[5]={0,10,20,30,40};
+void printALL (){
+    for(int i=0; i<5;i++)
+        printf("%d ",a[i]);
+    printf("\n");
+}
+int main() {
+            printALL ();
+    int *p = &a[2];
+        *p = 222;
+            printALL ();
+        p =p+2;
+        *p = 666;
+            printALL ();
+        p--;
+        *p = 555;
+            printALL ();
+
+}
