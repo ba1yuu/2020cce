47da4f2534cc2cdf7b3c68077fcb6d4812a84af5
ba1yuu <45198662+ba1yuu@users.noreply.github.com>
2021-05-14 11:33:34 +0800

    Add files via upload

---
+++ week12/hw6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int a[10000];
+int main()
+{
+    int n, m;
+    while(scanf("%d %d", &n, &m) == 2){
+        for(int i=0; i<n; i++){
+            scanf("%d", &a[i]);
+        }
+
+        printf("%d %d", n, m);
+        for(int i=0; i<n; i++){
+            printf("%d\n", a[i]);
+        }
+    }
+
+}
