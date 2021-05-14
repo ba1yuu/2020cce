48960204e6ee23141b59cddea74e335f47a30ecd
ba1yuu <45198662+ba1yuu@users.noreply.github.com>
2021-05-14 10:51:22 +0800

    Add files via upload

---
+++ week12/hw4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int a[100];
+int main()
+{
+    int T;
+    scanf("%d", &T);
+
+	for(int t=0; t<T ; t++)
+	{
+		int N;
+		scanf("%d", &N);
+		for(int i=0; i<N; i++){
+            scanf("%d", &a[i]);
+		}
+
+		int ans=0;
+
+		printf("Optiaml train swapping takes %d swaps.\n", ans);
+}
+}
